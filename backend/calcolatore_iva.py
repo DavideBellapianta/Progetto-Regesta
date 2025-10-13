4bd18737785d90ec430ddc3da849331410cbd7c4
DavideBellapianta <236595214+DavideBellapianta@users.noreply.github.com>
2025-10-13 09:26:40 +0200

    piccolo fix calcolatore

--- backend/calcolatore_iva.py
+++ backend/calcolatore_iva.py
@@ -1,13 +1,15 @@
-from decimal import Decimal, ROUND_DOWN, ROUND_UP
+from decimal import Decimal, ROUND_DOWN, ROUND_UP #Meglio decimal di flaot per essere più precisi
 
 ALIQUOTE_IVA = {
     "food": Decimal("0.04"),
     "medical": Decimal("0.10"),
-    "other": Decimal("0.22")
+    "other": Decimal("0.22"),
+    "casa": Decimal("0.22"),
+    "elettronica": Decimal("0.22"),
 }
 
 def arrotonda_a_005_inferiore(valore: Decimal) -> Decimal:
-    return (valore * 20).to_integral_value(rounding=ROUND_DOWN) / 20
+    return (valore * 20).to_integral_value(rounding=ROUND_DOWN) / 20 #Arrotondo per difetto tipo 1,28 * 20 = 25,6 si arrotonda a 25 poi / 20 = 1,25 
 
 def calcola_scontrino(carrello: list, collection_prodotti: object) -> dict:
     righe = []
@@ -15,7 +17,7 @@ def calcola_scontrino(carrello: list, collection_prodotti: object) -> dict:
     totale_tasse = Decimal("0.00")
 
     for item in carrello:
-        prodotto_db = collection_prodotti.find_one({"nome": item["nome"]})
+        prodotto_db = collection_prodotti.find_one({"nome": item["nome"]}) #Cerca per nome i prodotti
         if not prodotto_db:
             continue
 
@@ -23,17 +25,17 @@ def calcola_scontrino(carrello: list, collection_prodotti: object) -> dict:
         quantita = Decimal(item["quantita"])
         categoria = prodotto_db["categoria"]
         
-        aliquota_iva = ALIQUOTE_IVA.get(categoria, ALIQUOTE_IVA["other"])
+        aliquota_iva = ALIQUOTE_IVA.get(categoria, ALIQUOTE_IVA["other"]) #calcola il prezzo con iva tramite categorie
         
         prezzo_lordo_riga = prezzo_lordo * quantita
         prezzo_con_iva = prezzo_lordo_riga * (Decimal("1") + aliquota_iva)
         
-        prezzo_finale_arrotondato = arrotonda_a_005_inferiore(prezzo_con_iva)
+        prezzo_finale_arrotondato = arrotonda_a_005_inferiore(prezzo_con_iva) #arrotonda
         
         if prezzo_finale_arrotondato < prezzo_lordo_riga:
-             prezzo_finale_arrotondato = prezzo_lordo_riga.quantize(Decimal("0.01"), rounding=ROUND_UP)
+             prezzo_finale_arrotondato = prezzo_lordo_riga.quantize(Decimal("0.01"), rounding=ROUND_UP) #mi assicuro di non perdere valore (senza tasse)
 
-        tasse_riga = prezzo_finale_arrotondato - prezzo_lordo_riga
+        tasse_riga = prezzo_finale_arrotondato - prezzo_lordo_riga #Prezzo finale - prezzo senza tasse = tasse
         
         totale_netto += prezzo_finale_arrotondato
         totale_tasse += tasse_riga
